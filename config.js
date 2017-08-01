61b152851bf4c02cd21d823e0d279c9ba2b6d95c
joan.feng <joan.feng@pictureworks.biz>
2017-08-01 10:43:25 +0800

    参数阻挡

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 const config = {
-	dev:{
-		url : 'https://openapi.alipaydev.com/gateway.do?'
+	dev: {
+		url: 'https://openapi.alipaydev.com/gateway.do?'
 	},
-	product:{
-		url : 'https://openapi.alipay.com/gateway.do?'
+	product: {
+		url: 'https://openapi.alipay.com/gateway.do?'
+	},
+	param: {
+		'publicParam': ['app_id', 'method', 'biz_content', 'rsaKey']
 	}
 };
 
--- test/test.js
+++ test/test.js
@@ -3,17 +3,18 @@
 const moment = require('moment');
 const fs = require('fs');
 const alipay = require('../index.js').Alipay;
+
+//=================
 try {
-	let obj = {
-		env: 'https://openapi.alipaydev.com/gateway.do?',
-		app_id: '2016073100132509', //合作身份者id，以2088开头的16位纯数字
-		method: 'alipay.trade.pay',
-		charset: 'utf-8',
-		sign_type: 'RSA2',
-		format: 'JSON',
-		timestamp: moment().format('YYYY-MM-DD HH:mm:ss'),
+	let obj = { // <=====测试只选部分必填参数
+		env: 'dev', // 沙盒还是正式环境
+		app_id: '2016073100132509', // app_id : 必填
+		method: 'alipay.trade.pay', // 方法: 必填
+		charset: 'utf-8', // 默认:utf-8
+		sign_type: 'RSA2', // 默认:RSA2
+		timestamp: moment().format('YYYY-MM-DD HH:mm:ss'), // 默认当前时间
 		version: '1.0',
-		biz_content: {
+		biz_content: { // 请求参数: 必填
 			out_trade_no: 'ALIPAYTEST2016081622560194853',
 			scene: 'bar_code',
 			auth_code: '2016073100132509',
@@ -21,7 +22,8 @@ try {
 			total_amount: '0.01'
 		}
 	};
-	obj.rsaKey = fs.readFileSync('./key/app_private_key.pem').toString();
+	obj.rsaKey = fs.readFileSync('./key/app_private_key.pem').toString(); // 公钥或者私钥: 必填
+
 	alipay(obj, (result) => {
 		console.log(result.body);
 	});
