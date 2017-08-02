a641c9dbf0b0967b4f0f55c0cf5122f03869b715
joan.feng <joan.feng@pictureworks.biz>
2017-08-02 10:10:27 +0800

    config明确

--- config.js
+++ config.js
@@ -10,19 +10,21 @@ const config = {
 		'publicParam': ['app_id', 'method', 'biz_content', 'privateKey', 'publicKey'],
 
 		//各种API
+		'alipay.trade.precreate': ['out_trade_no', 'total_amount', 'subject'],
+		'alipay.trade.create': ['out_trade_no', 'total_amount', 'subject', 'goods_detail', ['buyer_id', 'buyer_logon_id']],
 		'alipay.trade.pay': ['out_trade_no', 'scene', 'auth_code', 'subject'],
-		'alipay.trade.fastpay.refund.query': ['out_request_no', ['trade_no', 'out_trade_no']],
-		'alipay.trade.order.settle': ['out_request_no', 'trade_no', 'royalty_parameters'],
-		'alipay.trade.close': ['operator_id', 'trade_no', 'out_trade_no'],
 		'alipay.trade.cancel': [
 			['out_trade_no', 'trade_no']
 		],
+		'alipay.trade.order.settle': ['out_request_no', 'trade_no', 'royalty_parameters'],
+
+		//交易辅助接口
+		'alipay.trade.close': ['operator_id', 'trade_no', 'out_trade_no'],
 		'alipay.trade.refund': ['refund_amount', ['out_trade_no', 'trade_no']],
-		'alipay.trade.precreate': ['out_trade_no', 'total_amount', 'subject'],
-		'alipay.trade.create': ['out_trade_no', 'total_amount', 'subject', 'goods_detail', ['buyer_id', 'buyer_logon_id']],
 		'alipay.trade.query': [
 			['out_trade_no', 'trade_no']
 		],
+		'alipay.trade.fastpay.refund.query': ['out_request_no', ['trade_no', 'out_trade_no']],
 
 		//手机网站支付
 		'alipay.trade.wap.pay': ['out_trade_no', 'total_amount', 'subject', 'product_code']
